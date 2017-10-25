0b0617cdafaf00dfd7297b9a70626457ac538f95
gizmore <gizmore@wechall.net>
2017-10-25 19:04:21 +0200

    sync

---
+++ thm/material/User/tpl/form/password.php
@@ -0,0 +1,16 @@
+<?php /** @var $field \GDO\User\GDT_Password **/ ?>
+<md-input-container class="md-block md-float md-icon-left<?= $field->classError(); ?>" flex>
+  <?= $field->icon; ?>
+  <label for="form[<?= $field->name; ?>]"><?= $field->label; ?></label>
+  <input
+   type="password"
+   <?= $field->htmlRequired(); ?>
+   <?= $field->htmlPattern(); ?>
+   <?= $field->htmlDisabled(); ?>
+   min="<?= $field->min; ?>"
+   max="<?= $field->max; ?>"
+   size="<?= min($field->max, 32); ?>"
+   name="form[<?= $field->name; ?>]"
+   value="<?= $field->getVar(); ?>" />
+  <?= $field->htmlError(); ?>
+</md-input-container>
