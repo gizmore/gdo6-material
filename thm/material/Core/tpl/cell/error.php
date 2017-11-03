cdaecd4685757f6572f2d4aff0347161ac58a99b
gizmore <gizmore@wechall.net>
2017-11-03 03:34:32 +0100

    6.06_fixes

--- thm/material/Core/tpl/cell/error.php
+++ thm/material/Core/tpl/cell/error.php
@@ -1,5 +1,5 @@
 <?php /** @var $field \GDO\Core\GDT_Error **/
 use GDO\UI\GDT_Icon; ?>
-<div class="gdo-success gdo-panel" flex layout-fill layout-padding>
+<div class="gdo-error gdo-panel" flex layout-fill layout-padding>
   <div class="md-whiteframe-8dp"><?=GDT_Icon::iconS('error')?><?= $field->html; ?></div>
 </div>
