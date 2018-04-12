06ad377219151336bcc92de69b250a2423e6db61
gizmore <gizmore@wechall.net>
2018-04-12 23:51:52 +0200

    6.07_fontawesome

--- thm/material/Table/tpl/filter/_table_order.php
+++
@@ -1,8 +0,0 @@
-<?php
-use GDO\Type\GDT_Base;
-$field instanceof GDT_Base;
-?>
-<div class="gdo-tblorder">
-  <a href="<?= $url_asc; ?>" class="asc<?= $is_asc ? ' sel' : ''; ?>"><i class="material-icons">arrow_drop_up</i></a>
-  <a href="<?= $url_desc; ?>" class="desc<?= $is_desc ? ' sel' : ''; ?>"><i class="material-icons">arrow_drop_down</i></a>
-</div>
