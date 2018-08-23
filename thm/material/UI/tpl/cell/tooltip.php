54db0bd5ff02c70b97d44b5da4d768ea75a25532
gizmore <gizmore@wechall.net>
2018-08-23 20:07:30 +0200

    6.08-r17_tinymce_pathes

--- thm/material/UI/tpl/cell/tooltip.php
+++ thm/material/UI/tpl/cell/tooltip.php
@@ -2,7 +2,9 @@
 use GDO\UI\GDT_Icon;
 use GDO\UI\GDT_Tooltip;
 $field instanceof GDT_Tooltip; ?>
+<?php if ($field->iconText) : ?>
 <md-button class="md-icon-button">
   <?=GDT_Icon::iconS('help')?>
-  <md-tooltip md-direction="right"><?= $field->iconText; ?></md-tooltip>
+  <md-tooltip md-direction="right" ><?= $field->iconText; ?></md-tooltip>
 </md-button>
+<?php endif; ?>
