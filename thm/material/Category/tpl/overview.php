354577621ac40fea2c059e937c4f26151df9454a
gizmore <gizmore@wechall.net>
2018-10-08 00:46:55 +0200

    6.08-r55-ws-auth

--- thm/material/Category/tpl/overview.php
+++ thm/material/Category/tpl/overview.php
@@ -6,7 +6,7 @@ use GDO\UI\GDT_Button;
 
 $gdo = GDO_Category::table();
 $module = Module_Category::instance();
-echo $module->renderAdminTabs(); 
+echo $module->renderAdminTabs()->render(); 
 
 # Render table with this query
 $query = $gdo->select('*');
