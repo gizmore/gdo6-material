da1297806fb14ca211c35e9115984adda8de5762
gizmore <gizmore@wechall.net>
2019-03-08 21:01:31 +0100

    6.09-r79_css

--- thm/material/UI/tpl/cell/list_item.php
+++ thm/material/UI/tpl/cell/list_item.php
@@ -4,12 +4,14 @@ $gdt->addClass('md-3-line');
 ?>
 <md-list-item <?=$gdt->htmlAttributes()?>>
 <?php if ($gdt->image) : ?>
-	<div class="gdt-li-image"><?=$gdt->image->renderCell()?></div>
+	<div class="gdt-li-image md-avatar"><?=$gdt->image->renderCell()?></div>
 <?php endif; ?>
   <div class="md-list-item-text" layout="column">
 	<?=$gdt->title->renderCell()?>
 	<?=$gdt->subtitle->renderCell()?>
 	<?=$gdt->subtext->renderCell()?>
   </div>
+  <div class="md-secondary">
   <?=$gdt->actions()->render()?>
+  </div>
 </md-list-item>
