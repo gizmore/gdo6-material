256c035495e99f3ff85433d7c2189f0c2979131d
gizmore <gizmore@wechall.net>
2021-03-05 22:26:14 +0100

    material_success_and_error

--- thm/material/Core/tpl/cell/success.php
+++ thm/material/Core/tpl/cell/success.php
@@ -1,5 +1,14 @@
 <?php /** @var $field \GDO\Core\GDT_Success **/
 use GDO\UI\GDT_Icon; ?>
 <div class="gdo-success gdo-panel" flex layout-fill layout-padding>
-<div class="md-whiteframe-8dp"><?=GDT_Icon::iconS('done')?><?= $field->html; ?></div>
+  <div class="md-whiteframe-8dp">
+<?php if ($field->hasTitle()) : ?>
+    <h3><?=GDT_Icon::iconS('check')?><?=$field->renderTitle()?></h3>
+  <?php if ($field->hasText()) : ?>
+    <p><?php $field->renderText()?></p>
+  <?php endif; ?>
+<?php else : ?>
+    <p><?=GDT_Icon::iconS('check')?><?=$field->renderText()?></p>
+<?php endif; ?>
+  </div>
 </div>
