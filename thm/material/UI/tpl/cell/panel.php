d551abc4d80e120913fd94b04223413cafc4c04c
gizmore <gizmore@wechall.net>
2021-10-20 18:33:48 +0200

    6.10.5-r6129

--- thm/material/UI/tpl/cell/panel.php
+++ thm/material/UI/tpl/cell/panel.php
@@ -1,6 +1,16 @@
 <?php /** @var $field \GDO\UI\GDT_Panel; **/ ?>
 <div layout="row" layout-fill layout-padding>
   <div class="md-whiteframe-2dp" layout-fill>
-	<?=$field->html?>
+<?php if ($field->hasTitle()) : ?>
+  <h3><?=$field->renderTitle()?></h3>
+<?php endif; ?>
+<?php if ($field->hasText()) : ?>
+  <p><?=$field->renderText()?></p>
+<?php endif; ?>
+<?php if ($field->fields) : ?>
+<?php foreach ($field->fields as $gdt) : ?>
+<?=$gdt->renderCell()?>
+<?php endforeach; ?>
+<?php endif; ?>
   </div>
 </div>
