5deb76670ec21a54cb4876756be6b62d17eb5454
gizmore <gizmore@wechall.net>
2021-10-31 06:05:55 +0100

    6.10.6-r6158_btn_fixes

--- thm/material/UI/tpl/cell/button.php
+++ thm/material/UI/tpl/cell/button.php
@@ -2,7 +2,7 @@
 use GDO\UI\GDT_Button;
 $field instanceof GDT_Button;
 $field->addClass("md-button md-raised");
-$field->addClass($field->primaryButton?"md-primary":"md-secondary");
+$field->addClass(!$field->priority?"md-primary":"md-secondary");
 ?>
 <a <?=$field->htmlAttributes()?> href="<?=$href?>" <?=$field->htmlDisabled()?>>
   <?=$field->displayLabel()?>
